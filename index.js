21c7af3e3f582355a0920fd8974c50b13f0eebfd
lipeng <lipeng@renrenche.com>
2016-12-07 15:45:44 +0800

    修改验证方式

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 var path = require('path');
+var jsonfile = require('jsonfile');
 var through2 = require('through2');
 var colors = require('gulp-util').colors;
 var log = require('gulp-util').log;
 var Q = require('q');
 var fs = require('fs');
+var qcloud = require('qcloud_cos');
 var existFiles = 0;
 var uploadedFiles = 0;
 var uploadedFail = 0;
-var qcloud = require('qcloud_cos');
+
+var qcloudJson = fs.existsSync('.qcloud.json');
+var obj = {'创建缓存文件': true};
+if(!qcloudJson){
+    jsonfile.writeFileSync('.qcloud.json', obj, {spaces: 4});
+}
+var qcloudMapJson = require('../../.qcloud.json');
 
 module.exports = function(cloud, option) {
     option = option || {};
-    option = Object.assign({}, { dir: '', prefix: '', cacheFile: './cacheQCloudList.txt'}, option);
-    var tasks = [],
-        newUploadFile = '';
+    option = Object.assign({}, { dir: '', prefix: ''}, option);
+    var tasks = [];
 
-    //设置超时时间
     var timeout = cloud.timeout || 30;
     qcloud.conf.setAppInfo(cloud.appid, cloud.secretId, cloud.accessId, timeout);
-
-    //创建缓存文件
-    fs.exists(option.cacheFile, function (exists) {
-        if(!exists){
-            fs.writeFileSync(option.cacheFile, '', 'utf8');
-        }
-    });
 
     return through2.obj(function(file, enc, next) {
         if (file._contents === null) {return next();}
@@ -35,37 +34,33 @@ module.exports = function(cloud, option) {
 
         var handler = function() {
             var defer = Q.defer();
-            fs.readFile(option.cacheFile, 'UTF-8', function (err, data) {
-                if (err){
-                    return defer.reject();
-                }
-                //文件在缓存文件中存在
-                if(data.indexOf(prefixFileKey) !== -1){
+
+            //文件在缓存文件中存在
+            if(qcloudMapJson[prefixFileKey]){
+                existFiles++;
+                return defer.resolve();
+            }
+
+            //保存未缓存文件
+            qcloudMapJson[prefixFileKey] = true;
+
+            //文件在缓存文件中不存在，再到远程判断是否存在 也是为了新使用时创建缓存文件
+            qcloud.cos.statFile(cloud.bucket, prefixFileKey, function(ret) {
+                //由于不在缓存文件中，添加到缓存文件中
+                jsonfile.writeFileSync('.qcloud.json', qcloudMapJson, {spaces: 4});
+                if (ret.code === 0) {
                     existFiles++;
                     return defer.resolve();
                 }
-
-                //拼接没有的新上传文件
-                newUploadFile = newUploadFile + prefixFileKey + ',';
-
-                //文件在缓存文件中不存在，再到远程判断是否存在 也是为了新使用时创建缓存文件
-                qcloud.cos.statFile(cloud.bucket, prefixFileKey, function(ret) {
-                    //由于不在缓存文件中，添加到缓存文件中
-                    fs.writeFileSync(option.cacheFile, data + newUploadFile, 'utf8');
-                    if (ret.code === 0) {
-                        existFiles++;
-                        return defer.resolve();
+                uploadedFiles++;
+                qcloud.cos.upload(fileKey, cloud.bucket, prefixFileKey, 1, function(ret) {
+                    log('Start →', colors.green(prefixFileKey), '→', ret.message);
+                    if (ret.code !== 0) {
+                        uploadedFail++;
+                        log('Error →', colors.red(prefixFileKey), ret.message);
+                        defer.reject();
                     }
-                    uploadedFiles++;
-                    qcloud.cos.upload(fileKey, cloud.bucket, prefixFileKey, 1, function(ret) {
-                        log('Start →', colors.green(prefixFileKey), '→', ret.message);
-                        if (ret.code !== 0) {
-                            uploadedFail++;
-                            log('Error →', colors.red(prefixFileKey), ret.message);
-                            defer.reject();
-                        }
-                        defer.resolve();
-                    });
+                    defer.resolve();
                 });
             });
             return defer.promise;
